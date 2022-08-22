d8b5baa6a88feef89eff8b35c79d7d6eb3002d2e
Jebin Joseph <jebin.joseph@outlook.com>
2022-08-22 15:24:46 -0400

    Screenshot java file

--- src/main/java/learnSelenium/ScreenshotDemo.java
+++ src/main/java/learnSelenium/ScreenshotDemo.java
@@ -1,30 +1,57 @@
 package learnSelenium;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Date;
 
+import javax.imageio.ImageIO;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 import utilities.SeleniumUtils;
 
 public class ScreenshotDemo {
+	
+	static WebDriver driver;
+	static String fileName;
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		try {
 			driver.get("https://wallethub.com/free-credit-score");
-			SeleniumUtils.captureScreenshot(driver);
-			
+			fileName = new Date().toString().replace(",", "_").replace(":", "_").replace(" ","_");
+			getScreenshot();
+			fullScreenshot();
 			
 		}catch (Exception e) {
 			e.printStackTrace();
 		
 
+	    }
+		
+ }
+	
+	public static void getScreenshot() throws IOException {
+		
+		File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshotFile, new File(".//screenshots//selenium_" + fileName + ".jpg"));
 	}
-
-}
+	
+	public static void fullScreenshot() throws IOException {
+		Screenshot sh = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+		ImageIO.write(sh.getImage(), "jpg",new File(".//screenshots//ashot_" + fileName + ".jpg"));
+	}
+	
+	
 }
