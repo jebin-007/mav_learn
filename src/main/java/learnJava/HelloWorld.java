b7b5695a69eb183b9fc5fbf1a47ef53c17a2e26d
jebin <jebin@Jebin-ZephG14>
2023-04-03 23:35:50 -0400

    Hello word learn

---
+++ src/main/java/learnJava/HelloWorld.java
@@ -0,0 +1,11 @@
+package learnJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("All is good");
+		
+	}
+
+}
